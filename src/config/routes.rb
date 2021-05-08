635475945d1ac46690e2e9a7eb54be27fc5e9116
kory-job <catcher.intherye.rk@gmail.com>
2021-05-08 10:40:38 +0900

    customer_header_create

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :customer do
     root "top#index"
+    get "login" => "sessions#new", as: :login
+    post "session" => "sessions#create", as: :session
+    delete "session" => "session#destory"
   end
 
   namespace :admin do
